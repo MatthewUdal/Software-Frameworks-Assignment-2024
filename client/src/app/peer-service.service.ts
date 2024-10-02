e6ded6c8bde87a3576786d80af1e5fb57aa76288
www-data <www-data@s5394035.elf.ict.griffith.edu.au>
2024-10-02 13:23:43 +1000

    peer client side init

--- client/src/app/peer-service.service.ts
+++ client/src/app/peer-service.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import Peer, { DataConnection, MediaConnection } from 'peerjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeerServiceService {
+  private peer: Peer | null = null;
+  private mediaConnection: MediaConnection | null = null;
+  private PORT1 = 3001;
 
   constructor() { }
+
+  // Initialize the PeerJS client and connect to the server
+  initPeer(userId: string) {
+    this.peer = new Peer(userId, {
+      host: 'http://s5394035.elf.ict.griffith.edu.au',
+      port: this.PORT1,
+      path: '/videocall',
+      secure: true
+    });
+
+    return this.peer;
+  }
+
+
 }
